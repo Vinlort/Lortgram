7c93a347cfd94b119e8b7b062dd6450f7a54fd75
Vinlort <vetal.nyzhnyk@gmail.com>
2023-03-20 17:47:48 +0200

    added new chat photos

--- app/src/main/java/com/vinlort/mychatapp/NewMessageActivity.kt
+++ app/src/main/java/com/vinlort/mychatapp/NewMessageActivity.kt
@@ -3,38 +3,29 @@ package com.vinlort.mychatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.google.firebase.auth.FirebaseAuth
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.vinlort.mychatapp.databinding.ActivityNewMessageBinding
-import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 
+private lateinit var binding: ActivityNewMessageBinding
 class NewMessageActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityNewMessageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewMessageBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.title = "Select User"
-/*
-        val adapter = GroupieAdapter()
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
 
-        binding.recyclerViewNewMessage.adapter = adapter
-*/
         fetchUsers()
     }
     private fun fetchUsers(){
@@ -46,7 +37,9 @@ class NewMessageActivity : AppCompatActivity() {
                 p0.children.forEach{
                     Log.d("NewMessage", it.toString())
                     val user = it.getValue(User::class.java)
-                    adapter.add(UserItem())
+                    if (user != null){
+                        adapter.add(UserItem(user))
+                    }
                 }
                 binding.recyclerViewNewMessage.adapter = adapter
             }
@@ -58,10 +51,11 @@ class NewMessageActivity : AppCompatActivity() {
     }
 }
 
-class UserItem: Item<GroupieViewHolder>() {
+class UserItem(val user: User): Item<GroupieViewHolder>() {
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-
+        viewHolder.itemView.findViewById<TextView>(R.id.username_textview_new_message).text = user.username
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.findViewById<ImageView>(R.id.imageview_new_message))
     }
     override fun getLayout():Int {
         return R.layout.user_row_new_message
